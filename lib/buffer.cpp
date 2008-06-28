3afc69fde7db3487e943fb108368eaf15ddd0bfc
Hervé Fache <herve@lucidia.net>
2008-06-28 08:46:13 +0000

    buffer: make it multithread-safe without locks

--- lib/buffer.cpp
+++ lib/buffer.cpp
@@ -43,7 +43,7 @@ int Buffer::unregisterReader(const BufferReader* reader) {
 }
 
 Buffer::Buffer(size_t size) {
-  _write_id = 0;
+  _write_count = 0;
   _readers_size = 0;
   if (size > 0) {
     create(size);
@@ -61,6 +61,7 @@ Buffer::~Buffer() {
 void Buffer::create(size_t size) {
   _buffer_start = static_cast<char*>(malloc(size));
   _buffer_end   = &_buffer_start[size];
+  _capacity     = size;
   empty();
 }
 
@@ -70,9 +71,10 @@ void Buffer::destroy() {
 }
 
 void Buffer::empty() {
-  _write_start = _buffer_start;
-  _write_end   = _buffer_start;
-  _full        = false;
+  _write_start    = _buffer_start;
+  _write_count    = 0;
+  _read_count     = 0;
+  _readers_update = false;
   for (std::list<BufferReader*>::iterator i = _readers.begin();
       i != _readers.end(); i++) {
     (*i)->empty();
@@ -80,66 +82,20 @@ void Buffer::empty() {
 }
 
 void Buffer::update() {
-  int min_free = _buffer_end - _buffer_start;
+  int max_used = 0;
   for (std::list<BufferReader*>::iterator i = _readers.begin();
       i != _readers.end(); i++) {
-    // Buffer free space for this reader
-    int this_free = (*i)->reader() - _write_start;
-    // Wrapped
-    if (this_free < 0) {
-      this_free += _buffer_end - _buffer_start;
-    }
-    // Better or empty
-    if ((this_free < min_free) && ((this_free != 0) || ! (*i)->isEmpty())) {
-      min_free = this_free;
+    // Unread space for this reader
+    int this_used = _write_count - (*i)->_read_count;;
+    if (this_used > max_used) {
+      max_used = this_used;
     }
   }
-  // Compute new end
-  _write_end = _write_start + min_free;
-  if (_write_end >= _buffer_end) {
-    _write_end -= _buffer_end - _buffer_start;
-  }
-  _full = (min_free == 0);
+  _read_count = _write_count - max_used;
 }
 
 bool Buffer::exists() const {
   return _buffer_start != NULL;
-}
-
-size_t Buffer::usage() const {
-  if (_write_start > _write_end) {
-    return _write_start - _write_end;
-  } else
-  if ((_write_start < _write_end) || _full) {
-    return _buffer_end - _buffer_start + _write_start - _write_end;
-  } else
-  {
-    return 0;
-  }
-}
-
-size_t Buffer::writeable() const {
-  if (_full) {
-    return 0;
-  } else
-  if (_write_start < _write_end) {
-    return _write_end - _write_start;
-  } else
-  {
-    return _buffer_end - _write_start;
-  }
-}
-
-void Buffer::written(size_t size) {
-  if (size == 0) return;
-  _write_start += size;
-  if (_write_start >= _buffer_end) {
-    _write_start = _buffer_start;
-  }
-  if (_write_start == _write_end) {
-    _full = true;
-  }
-  _write_id++;
 }
 
 ssize_t Buffer::write(const void* buffer, size_t size) {
@@ -159,46 +115,6 @@ ssize_t Buffer::write(const void* buffer, size_t size) {
     size -= really;
   }
   return really;
-}
-
-void BufferReader::empty() {
-  _read_start = _buffer._write_end;
-  _empty_id   = _buffer._write_id;
-}
-
-size_t BufferReader::readable() const {
-  // The readable/written part is [_write_end _write_start[
-  if (_read_start < _buffer._write_start) {
-    return _buffer._write_start - _read_start;
-  } else
-  // Wrapped
-  if ((_read_start > _buffer._write_start) || _buffer._full) {
-    return _buffer._buffer_end - _read_start;
-  } else
-  // Empty
-  {
-    return 0;
-  }
-}
-
-void BufferReader::readn(size_t size) {
-  if (size == 0) return;
-  _read_start += size;
-  if (_read_start >= _buffer._buffer_end) {
-    _read_start = _buffer._buffer_start;
-  }
-  // Read it all?
-  if (_read_start == _buffer._write_start) {
-    _empty_id = _buffer._write_id;
-  }
-  if (_buffer._readers_size == 1) {
-    // Just update
-    _buffer._write_end = _read_start;
-    _buffer._full      = false;
-  } else {
-    // Tell write to find new end
-    _buffer.update();
-  }
 }
 
 ssize_t BufferReader::read(void* buffer, size_t size) {
--- lib/buffer.h
+++ lib/buffer.h
@@ -34,13 +34,14 @@ class BufferReader;
 class Buffer {
   char*             _buffer_start;
   const char*       _buffer_end;
+  unsigned int      _capacity;
   char*             _write_start;
-  const char*       _write_end;
-  unsigned int      _write_id;
-  bool              _full;
+  unsigned int      _write_count;
+  unsigned int      _read_count;
   std::list<BufferReader*>
                     _readers;
   unsigned int      _readers_size;
+  bool              _readers_update;
   friend class      BufferReader;
   // Registering
   void registerReader(BufferReader* reader);
@@ -59,36 +60,59 @@ public:
     \param size         size of the buffer to create
   */
   void create(size_t size = 102400);
+    // Multithread irrelevant: must be done synchronously
   //! \brief Destroy
   void destroy();
+    // Multithread irrelevant: must be done synchronously
   //! \brief Empty the buffer of all content
   void empty();
+    // Multithread irrelevant: must be done synchronously
+  //! \brief Check whether the write limit can be increased
+  inline void reader_update(unsigned int reader_count) {
+    // Multithread safe
+    if (_readers_size == 1) {
+      _read_count = reader_count;
+    } else {
+      _readers_update = true;
+    }
+  }
   //! \brief Check whether the write limit can be increased
   void update();
+    // Multithread safe
   //! \brief Check whether the buffer was created
   /*!
       \return           true if was created, false otherwise
   */
   bool exists() const;
+    // Multithread safe
   //! \brief Get buffer's capacity
   /*!
       \return           total buffer capacity
   */
   inline size_t capacity() const {
-    return _buffer_end - _buffer_start;
+    // Multithread safe
+    return _capacity;
   }
   //! \brief Get buffer's current usage
   /*!
       \return           buffer current usage
   */
-  size_t usage() const;
+  inline size_t usage() {
+    // Multithread safe
+    if (_readers_update) {
+      update();
+      _readers_update = false;
+    }
+    return _write_count - _read_count;
+  }
   // Status
   //! \brief Check whether the buffer is full
   /*!
       \return           true if full, false otherwise
   */
-  inline bool isFull() const {
-    return _full;
+  inline bool isFull() {
+    // Multithread safe
+    return usage() == _capacity;
   }
   // Writing
   //! \brief Get pointer to where to write
@@ -96,18 +120,33 @@ public:
       \return           pointer to where to write
   */
   inline char* writer() {
+    // Multithread safe
     return _write_start;
   }
   //! \brief Get buffer's contiguous free space
   /*!
       \return           size of contiguous free space
   */
-  size_t writeable() const;
+  inline size_t writeable() {
+    // Multithread safe
+    unsigned int total_free    = _capacity - usage();
+    unsigned int straight_free = _buffer_end - _write_start;
+    return (straight_free < total_free) ? straight_free : total_free;
+  }
   //! \brief Set how much space was used
   /*!
     \param size         size written
   */
-  void written(size_t size);
+  inline void written(size_t size) {
+    // Multithread safe
+    // Update counter
+    _write_count += size;
+    // Update pointer
+    _write_start += size;
+    if (_write_start >= _buffer_end) {
+      _write_start = _buffer_start;
+    }
+  }
   //! \brief Write data from an external buffer
   /*!
     \param buffer       pointer to the external buffer
@@ -115,51 +154,77 @@ public:
     \return             size actually written
   */
   ssize_t write(const void* buffer, size_t size);
+    // Multithread safe: only uses other methods
 };
 
 class BufferReader {
   Buffer&           _buffer;
   const char*       _read_start;
-  unsigned int      _empty_id;
+  unsigned int      _read_count;
+  friend class      Buffer;
 public:
   //! \brief Constructor
   /*!
     \param buffer       buffer to read from
   */
   BufferReader(Buffer& buffer) : _buffer(buffer) {
+    // Multithread irrelevant: must be done synchronously
     _buffer.registerReader(this);
     empty();
   }
   //! \brief Destructor
   ~BufferReader() {
+    // Multithread irrelevant: must be done synchronously
     _buffer.unregisterReader(this);
   }
   //! \brief Empty the buffer of all content
-  void empty();
+  inline void empty() {
+    // Multithread irrelevant: must be done synchronously
+    _read_start = _buffer._write_start;
+    _read_count = _buffer._write_count;
+  }
   //! \brief Check whether the buffer is empty
   /*!
       \return           true if empty, false otherwise
   */
   inline bool isEmpty() const {
-    return _empty_id == _buffer._write_id;
+    // Multithread safe: only _buffer._write_count can change, read only once
+    return _read_count == _buffer._write_count;
   }
   //! \brief Get pointer to where to read
   /*!
       \return           pointer to where to read
   */
   inline const char* reader() const {
+    // Multithread safe: nothing can change
     return _read_start;
   }
   //! \brief Get buffer's contiguous used space
   /*!
       \return           size of contiguous used space
   */
-  size_t readable() const;
+  inline size_t readable() const {
+    // Multithread safe: only _buffer._write_count can change, read only once
+    unsigned int total_free    = _buffer._write_count - _read_count;
+    unsigned int straight_free = _buffer._buffer_end - _read_start;
+    return (straight_free < total_free) ? straight_free : total_free;
+  }
   //! \brief Set how much space was freed
   /*!
     \param size         size read
   */
-  void readn(size_t size);
+  inline void readn(size_t size) {
+    // Multithread safe: nothing can change
+    // Update counter
+    _read_count += size;
+    // Tell write to find new end
+    _buffer.reader_update(_read_count);
+    // Update pointer
+    _read_start += size;
+    if (_read_start >= _buffer._buffer_end) {
+      _read_start = _buffer._buffer_start;
+    }
+  }
   //! \brief Read data to an external buffer
   /*!
     \param buffer       pointer to the external buffer
@@ -167,6 +232,7 @@ public:
     \return             size actually read
   */
   ssize_t read(void* buffer, size_t size);
+    // Multithread safe: only uses other methods
 };
 
 }
