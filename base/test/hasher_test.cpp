978ea77da8edf73531591ce476f2ea89fe0ed0b7
Hervé Fache <herve@lucidia.net>
2010-09-26 14:13:00 +0000

    hasher: also test sha1, sha256 and sha512

--- base/test/hasher_test.cpp
+++ base/test/hasher_test.cpp
@@ -75,7 +75,7 @@ int main() {
     size_t size;
 
     FileReaderWriter frw("testfile", false);
-    char hash[64];
+    char hash[129];
     Hasher hh(&frw, false, Hasher::md5, hash);
     IReaderWriter& fd = hh;
     if (fd.open() < 0) {
@@ -125,6 +125,288 @@ int main() {
       }
     }
     system("md5sum testfile2");
+  }
+
+  {
+    FileReaderWriter frw("testfile", true);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha1, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      char buffer[5000];
+      memset(buffer, 0x41, sizeof(buffer));
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        size_t size;
+        if (count == 0) {
+          size = 3000;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.write(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s writing file", strerror(errno));
+        } else {
+          hlog_regression("written %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha1sum testfile");
+  }
+
+  {
+    char buffer[6000];
+    memset(buffer, 0, sizeof(buffer));
+    size_t size;
+
+    FileReaderWriter frw("testfile", false);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha1, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        if (count == 0) {
+          size = 4096;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.read(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s reading file", strerror(errno));
+        } else {
+          hlog_regression("read %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+      size = count;
+    }
+
+    FileReaderWriter frw2("testfile2", true);
+    Hasher hh2(&frw2, false, Hasher::sha1, hash);
+    IReaderWriter& fd2 = hh2;
+    if (fd2.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      rc = fd2.write(buffer, size);
+      if (rc < 0) {
+        hlog_regression("%s writing file", strerror(errno));
+      } else {
+        hlog_regression("written %zd bytes", rc);
+      }
+      if (fd2.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha1sum testfile2");
+  }
+
+  {
+    FileReaderWriter frw("testfile", true);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha256, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      char buffer[5000];
+      memset(buffer, 0x41, sizeof(buffer));
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        size_t size;
+        if (count == 0) {
+          size = 3000;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.write(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s writing file", strerror(errno));
+        } else {
+          hlog_regression("written %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha256sum testfile");
+  }
+
+  {
+    char buffer[6000];
+    memset(buffer, 0, sizeof(buffer));
+    size_t size;
+
+    FileReaderWriter frw("testfile", false);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha256, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        if (count == 0) {
+          size = 4096;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.read(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s reading file", strerror(errno));
+        } else {
+          hlog_regression("read %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+      size = count;
+    }
+
+    FileReaderWriter frw2("testfile2", true);
+    Hasher hh2(&frw2, false, Hasher::sha256, hash);
+    IReaderWriter& fd2 = hh2;
+    if (fd2.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      rc = fd2.write(buffer, size);
+      if (rc < 0) {
+        hlog_regression("%s writing file", strerror(errno));
+      } else {
+        hlog_regression("written %zd bytes", rc);
+      }
+      if (fd2.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha256sum testfile2");
+  }
+
+  {
+    FileReaderWriter frw("testfile", true);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha512, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      char buffer[5000];
+      memset(buffer, 0x41, sizeof(buffer));
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        size_t size;
+        if (count == 0) {
+          size = 3000;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.write(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s writing file", strerror(errno));
+        } else {
+          hlog_regression("written %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha512sum testfile");
+  }
+
+  {
+    char buffer[6000];
+    memset(buffer, 0, sizeof(buffer));
+    size_t size;
+
+    FileReaderWriter frw("testfile", false);
+    char hash[64];
+    Hasher hh(&frw, false, Hasher::sha512, hash);
+    IReaderWriter& fd = hh;
+    if (fd.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      size_t count = 0;
+      do {
+        if (count == 0) {
+          size = 4096;
+        } else {
+          size = sizeof(buffer) - count;
+        }
+        rc = fd.read(&buffer[count], size);
+        if (rc < 0) {
+          hlog_regression("%s reading file", strerror(errno));
+        } else {
+          hlog_regression("read %zd bytes", rc);
+        }
+        count += rc;
+      } while (rc > 0);
+      if (fd.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+      size = count;
+    }
+
+    FileReaderWriter frw2("testfile2", true);
+    Hasher hh2(&frw2, false, Hasher::sha512, hash);
+    IReaderWriter& fd2 = hh2;
+    if (fd2.open() < 0) {
+      hlog_regression("%s opening file", strerror(errno));
+    } else {
+      ssize_t rc;
+      rc = fd2.write(buffer, size);
+      if (rc < 0) {
+        hlog_regression("%s writing file", strerror(errno));
+      } else {
+        hlog_regression("written %zd bytes", rc);
+      }
+      if (fd2.close() < 0) {
+        hlog_regression("%s closing file", strerror(errno));
+      } else {
+        hlog_regression("hash = '%s'", hash);
+      }
+    }
+    system("sha512sum testfile2");
   }
 
   return 0;
