7dd00525a0aa4dc1ab810243d409e082aa9ad92a
Hervé Fache <herve@lucidia.net>
2010-11-28 00:18:43 +0000

    server: handle dynamic thread destruction

--- base/test/work_scheduler_test.cpp
+++ base/test/work_scheduler_test.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <hreport.h>
 #include <job_queue.h>
@@ -22,13 +23,15 @@ int main(void) {
   report.addRegressionCondition("work_scheduler.cpp");
   report.addRegressionCondition("work_scheduler_test.cpp");
 
-  hlog_regression("no thread limit");
   JobQueue q_in("in");
-  q_in.open();
   JobQueue q_out("out");
-  q_out.open(20);
-  char user[32] = "user";
+  char user[32] = "";
   WorkScheduler ws(q_in, q_out, task, user);
+
+  hlog_regression("no thread limit");
+  q_in.open();
+  q_out.open(20);
+  strcpy(user, "user1");
   if (ws.start() != 0) {
     hlog_error("start failed");
   } else {
@@ -39,6 +42,7 @@ int main(void) {
     char data2[32] = "data2";
     q_in.push(data2);
     usleep(150000);
+    hlog_regression("%zu thread(s)", ws.threads());
     ws.stop();
     usleep(50000);
     char* data_out;
@@ -53,6 +57,7 @@ int main(void) {
   hlog_regression("thread limit = 1");
   q_in.open();
   q_out.open(20);
+  strcpy(user, "user2");
   if (ws.start(1) != 0) {
     hlog_error("start failed");
   } else {
@@ -63,6 +68,7 @@ int main(void) {
     char data2[32] = "data2";
     q_in.push(data2);
     usleep(100000);
+    hlog_regression("%zu thread(s)", ws.threads());
     ws.stop();
     usleep(100000);
     char* data_out;
@@ -73,5 +79,56 @@ int main(void) {
     }
     q_out.wait();
   }
+
+  hlog_regression("no thread limit, 10 objects");
+  q_in.open();
+  q_out.open(20);
+  strcpy(user, "user3");
+  if (ws.start() != 0) {
+    hlog_error("start failed");
+  } else {
+    usleep(50000);
+    char data1[32] = "data1";
+    q_in.push(data1);
+    usleep(250000);
+    char data2[32] = "data2";
+    q_in.push(data2);
+    usleep(250000);
+    char data3[32] = "data3";
+    q_in.push(data3);
+    usleep(250000);
+    char data4[32] = "data4";
+    q_in.push(data4);
+    usleep(250000);
+    char data5[32] = "data5";
+    q_in.push(data5);
+    usleep(250000);
+    char data6[32] = "data6";
+    q_in.push(data6);
+    usleep(250000);
+    char data7[32] = "data7";
+    q_in.push(data7);
+    usleep(250000);
+    char data8[32] = "data8";
+    q_in.push(data8);
+    usleep(250000);
+    char data9[32] = "data9";
+    q_in.push(data9);
+    usleep(250000);
+    char data10[32] = "data10";
+    q_in.push(data10);
+    usleep(150000);
+    hlog_regression("%zu thread(s)", ws.threads());
+    ws.stop();
+    usleep(50000);
+    char* data_out;
+    hlog_regression("out queue:");
+    q_out.close();
+    while (q_out.pop(reinterpret_cast<void**>(&data_out)) == 0) {
+      hlog_regression("  '%s'", data_out);
+    }
+    q_out.wait();
+  }
+
   return 0;
 }
--- base/work_scheduler.cpp
+++ base/work_scheduler.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <pthread.h>
 
 #include <list>
@@ -26,7 +27,7 @@ struct WorkerThreadData {
   pthread_t                 tid;
   char                      name[NAME_SIZE];
   JobQueue                  q_in;
-  bool                      busy;
+  time_t                    last_run;
   WorkerThreadData(WorkSchedulerData& p, const char* n)
   : parent(p), q_in(n) {
     strcpy(name, n);
@@ -42,10 +43,15 @@ struct WorkSchedulerData {
   void*                     user;
   size_t                    min_threads;
   size_t                    max_threads;
+  size_t                    threads;
+  time_t                    time_out;
+  bool                      running;
   // Local data
   pthread_mutex_t           threads_list_lock;
-  list<WorkerThreadData*>   threads;
-  WorkSchedulerData(JobQueue& in, JobQueue& out) : q_in(in), q_out(out) {
+  list<WorkerThreadData*>   busy_threads;
+  list<WorkerThreadData*>   idle_threads;
+  WorkSchedulerData(JobQueue& in, JobQueue& out)
+  : q_in(in), q_out(out), time_out(600), running(false) {
     pthread_mutex_init(&threads_list_lock, NULL);
   }
   ~WorkSchedulerData() {
@@ -54,10 +60,9 @@ struct WorkSchedulerData {
 };
 
 struct WorkScheduler::Private {
-  bool                      started;
   WorkSchedulerData         data;
   pthread_t                 monitor_tid;
-  Private(JobQueue& in, JobQueue& out) : started(false), data(in, out) {}
+  Private(JobQueue& in, JobQueue& out) : data(in, out) {}
 };
 
 static void* worker_thread(void* data) {
@@ -75,12 +80,15 @@ static void* worker_thread(void* data) {
       if (data_out != NULL) {
         d->parent.q_out.push(data_out);
       }
-      // Put idle thread at the front of the list
+      // Put idle thread at the front of the list, if not shutting down
       pthread_mutex_lock(&d->parent.threads_list_lock);
-      d->parent.threads.remove(d);
-      d->parent.threads.push_front(d);
+      if (d->parent.running) {
+        d->parent.busy_threads.remove(d);
+        // Always push to back so unused threads are at the front
+        d->parent.idle_threads.push_back(d);
+        d->last_run = time(NULL);
+      }
       pthread_mutex_unlock(&d->parent.threads_list_lock);
-      d->busy = false;
     } else {
       // Exit loop
       hlog_regression("%s.loop exit", d->name);
@@ -96,70 +104,94 @@ static void* monitor_thread(void* data) {
   size_t order = 0;
   // Loop
   while (true) {
-    WorkerThreadData* wtd;
     hlog_regression("monitor.loop enter");
     void* data_in;
     if (d->q_in.pop(&data_in) == 0) {
       pthread_mutex_lock(&d->threads_list_lock);
-      if (! d->threads.empty()) {
-        wtd = d->threads.front();
-      } else {
-        wtd = NULL;
-      }
-      // Create new thread if needed and possible
-      if ((wtd == NULL) ||
-          (wtd->busy && ((d->max_threads == 0) ||
-                         (d->threads.size() < d->max_threads)))) {
+      // Get first idle thread
+      WorkerThreadData* wtd = NULL;
+      if (! d->idle_threads.empty()) {
+        wtd = d->idle_threads.back();
+        d->idle_threads.pop_back();
+        // Always push to back so longest running threads are at the front
+        d->busy_threads.push_back(wtd);
+        // Get rid of oldest idle thread if too old
+        list<WorkerThreadData*>::iterator it = d->idle_threads.begin();
+        if ((it != d->idle_threads.end()) &&
+            (((*it)->last_run - time(NULL)) > d->time_out)) {
+          (*it)->q_in.close();
+          pthread_join((*it)->tid, NULL);
+          delete *it;
+          d->idle_threads.erase(it);
+          --d->threads;
+        }
+      } else
+      // Create new thread if possible
+      if (((d->max_threads == 0) || (d->busy_threads.size() < d->max_threads))) {
         // Create worker thread
         char name[NAME_SIZE];
         snprintf(name, NAME_SIZE, "worker #%zu", ++order);
         wtd = new WorkerThreadData(*d, name);
         if (pthread_create(&wtd->tid, NULL, worker_thread, wtd) == 0) {
-          d->threads.push_back(wtd);
+          d->busy_threads.push_back(wtd);
+          ++d->threads;
           hlog_regression("%s.thread created", wtd->name);
         } else {
-          // TODO Report error
+          // FIXME Report cause of error
+          hlog_error("could not create thread");
           delete wtd;
+          wtd = d->busy_threads.front();
+          d->busy_threads.pop_front();
+          d->busy_threads.push_back(wtd);
         }
+      } else
+      {
+        // Give longest running thread, and push it back
+        wtd = d->busy_threads.front();
+        d->busy_threads.pop_front();
+        d->busy_threads.push_back(wtd);
       }
       // Put data in thread queue
-      wtd->busy = true;
       wtd->q_in.push(data_in);
       pthread_mutex_unlock(&d->threads_list_lock);
     } else {
       hlog_regression("monitor.queue closed");
       pthread_mutex_lock(&d->threads_list_lock);
-      // Stop all threads
-      for (list<WorkerThreadData*>::iterator it = d->threads.begin();
-           it != d->threads.end(); ++it) {
+      d->running = false;
+      // Stop all idle threads
+      list<WorkerThreadData*>::iterator it = d->idle_threads.begin();
+      while (it != d->idle_threads.end()) {
+        (*it)->q_in.close();
+        hlog_regression("%s.queue closed", (*it)->name);
+        pthread_join((*it)->tid, NULL);
+        hlog_regression("%s.thread joined", (*it)->name);
+        delete *it;
+        it = d->idle_threads.erase(it);
+      }
+      hlog_regression("all idle worker thread(s) destroyed");
+      // Close all busy threads queues
+      for (list<WorkerThreadData*>::iterator it = d->busy_threads.begin();
+           it != d->busy_threads.end(); ++it) {
         (*it)->q_in.close();
         hlog_regression("%s.queue closed", (*it)->name);
       }
-      pthread_mutex_unlock(&d->threads_list_lock);
-      hlog_regression("all %zu worker queue(s) closed", d->threads.size());
-      // Wait for all to have stopped
-      wtd = NULL;
-      while (true) {
-        pthread_mutex_lock(&d->threads_list_lock);
-        list<WorkerThreadData*>::iterator it = d->threads.begin();
-        if (it == d->threads.end()) {
-          wtd = NULL;
-        } else {
-          wtd = *it;
-        }
+      hlog_regression("all %zu busy worker queue(s) closed",
+        d->busy_threads.size());
+      // Wait for all to have stopped (not put in idle list when shutting down)
+      it = d->busy_threads.begin();
+      while (it != d->busy_threads.end()) {
+        // Unlock so threads can finish
         pthread_mutex_unlock(&d->threads_list_lock);
-        if (wtd == NULL) {
-          break;
-        }
-        pthread_join(wtd->tid, NULL);
-        pthread_mutex_lock(&d->threads_list_lock);
-        d->threads.remove(wtd);
-        pthread_mutex_unlock(&d->threads_list_lock);
+        pthread_join((*it)->tid, NULL);
         if (hlog_is_worth(regression)) {
           usleep(100000);
-          hlog_regression("%s.thread joined", wtd->name);
+          hlog_regression("%s.thread joined", (*it)->name);
         }
+        delete *it;
+        pthread_mutex_lock(&d->threads_list_lock);
+        it = d->busy_threads.erase(it);
       }
+      pthread_mutex_unlock(&d->threads_list_lock);
       hlog_regression("all worker thread(s) joined");
       // Exit loop
       hlog_regression("monitor.loop exit");
@@ -183,30 +215,31 @@ WorkScheduler::~WorkScheduler() {
 }
 
 int WorkScheduler::start(size_t max_threads, size_t min_threads) {
-  if (_d->started) return -1;
+  if (_d->data.running) return -1;
   _d->data.min_threads = min_threads;
   _d->data.max_threads = max_threads;
   // Start monitoring thread
   int rc = pthread_create(&_d->monitor_tid, NULL, monitor_thread, &_d->data);
   if (rc == 0) {
-    _d->started = true;
+    _d->data.running = true;
+    _d->data.threads = 0;
     hlog_regression("monitor.thread created");
   }
   return rc;
 }
 
 int WorkScheduler::stop() {
-  if (! _d->started) return -1;
+  if (! _d->data.running) return -1;
 
   // Stop monitoring thread
   _d->data.q_in.close();
   pthread_join(_d->monitor_tid, NULL);
   hlog_regression("monitor.thread joined");
-  _d->started = false;
+  _d->data.running = false;
 
   return 0;
 }
 
 size_t WorkScheduler::threads() const {
-  return _d->data.threads.size();
+  return _d->data.threads;
 }
