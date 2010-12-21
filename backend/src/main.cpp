3a179338891b0088b04dc94fa1f5400ff4b28e10
Hervé Fache <herve@lucidia.net>
2010-12-21 13:50:19 -0200

    backend: add log, config and test

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -16,11 +16,80 @@
      Boston, MA 02111-1307, USA.
 */
 
+#include <stdint.h>
+
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+#include <report.h>
+#include <configuration.h>
+
+using namespace htools;
+
 #include <hbackup.h>  // progress_f (needs architecture review)
 #include <data.h>
 
 using namespace hbackup;
 
+// static const char *default_config_path = "/etc/hbackup/hbackend.conf";
+static const char *default_config_path = "../hbackend.conf";
+
+struct LocalConfig : ConfigObject {
+  string    hostname;
+  uint16_t  port;
+  LocalConfig() : port(0) {}
+  ConfigObject* configChildFactory(
+    const vector<string>& params,
+    const char*           path,
+    size_t                line_no);
+  void show() {
+    hlog_verbose("hostname = '%s'", hostname.c_str());
+    hlog_verbose("port     = %d", port);
+  }
+};
+
+ConfigObject* LocalConfig::configChildFactory(
+    const vector<string>& params,
+    const char*           path,
+    size_t                line_no) {
+  ConfigObject* co = NULL;
+  const string& keyword = params[0];
+  if (keyword == "hostname") {
+    hostname = params[1];
+    co = this;
+  } else
+  if (keyword == "port") {
+    port = static_cast<uint16_t>(atoi(params[1].c_str()));
+    co = this;
+  } else
+  {
+    hlog_error("%s:%zu: unknown keyword '%s'", path, line_no, keyword.c_str());
+  }
+  return co;
+}
+
+int read_config(const char* path, LocalConfig& conf) {
+  Config config;
+  config.add(config.root(), "hostname", 0, 1, 1);
+  config.add(config.root(), "port", 0, 1, 1);
+  hlog_debug("Reading configuration file '%s'", path);
+  ssize_t rc = config.read(path, 0, &conf);
+  if (rc < 0) {
+    return -1;
+  }
+  return 0;
+}
+
 int main(void) {
+  report.setLevel(debug);
+
+  LocalConfig config;
+  if (read_config(default_config_path, config) < 0) {
+    return 1;
+  }
+  config.show();
   return 0;
 }
